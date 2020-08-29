082dc6a54981b688f02e61f7706df2eb20e180fe
AnimeAllstar <ashcarter.awesome@gmail.com>
2020-08-29 19:17:56 +0530

    Code Cleanup

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,6 @@ from .views import (
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog-home"),
-    #path("", views.home, name="blog-home"),
     path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
